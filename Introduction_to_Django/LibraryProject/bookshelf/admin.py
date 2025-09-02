41490e7f92071feaecd435dbe7daee37a90c86a5
mhabtom <meski123habtom@gmail.com>
2025-09-03 00:14:59 +0200

    task 2 admin user bookstore app

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # columns shown in the list page
+    list_display = ("title", "author", "publication_year")
+    # right-side filters
+    list_filter = ("author", "publication_year")
+    # search box (top-right)
+    search_fields = ("title", "author")
+    # optional niceties
+    ordering = ("title",)
+    list_per_page = 25
