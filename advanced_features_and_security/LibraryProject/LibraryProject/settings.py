781c94d8544a63f9108ecb4ca377439ae872590f
mhabtom <meski123habtom@gmail.com>
2025-09-14 19:49:46 +0200

    task 1 custom user model settings.py fix

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "list_books"
 LOGOUT_REDIRECT_URL = "login"
+
+# Custom user model
+AUTH_USER_MODEL = "bookshelf.CustomUser"
