121d478335c7684f6e974024af5d20cb1675ca5f
mhabtom <meski123habtom@gmail.com>
2025-10-04 12:01:28 +0200

    Initial Setup and Project Configuration for a Django Blog

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,8 @@
+# blog/urls.py
+from django.urls import path
+from . import views  
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+]
