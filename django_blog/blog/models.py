eeca4817601b67d1ab03af99395f3d83d5147b04
mhabtom <meski123habtom@gmail.com>
2025-10-05 21:33:46 +0200

    Fix: add User import and Post model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.conf import settings
 from django.urls import reverse
 
+from django.contrib.auth.models import User 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='posts'
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
 
 class Meta:
     ordering = ['-published_date']
