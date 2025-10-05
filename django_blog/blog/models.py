83c6a13f3da9df177d5acd588324e8c1abfacdc5
mhabtom <meski123habtom@gmail.com>
2025-10-05 21:23:01 +0200

    merge fixed

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,31 +1,17 @@
-<<<<<<< HEAD
-# blog/models.py
-from django.conf import settings
-from django.db import models
-=======
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
->>>>>>> 121d478 (Initial Setup and Project Configuration for a Django Blog)
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-<<<<<<< HEAD
-        settings.AUTH_USER_MODEL,  # Django’s User model
-=======
         settings.AUTH_USER_MODEL,
->>>>>>> 121d478 (Initial Setup and Project Configuration for a Django Blog)
         on_delete=models.CASCADE,
         related_name='posts'
     )
 
-<<<<<<< HEAD
-    def __str__(self):
-        return self.title
-=======
 class Meta:
     ordering = ['-published_date']
 
@@ -34,4 +20,3 @@ def __str__(self):
 
 def get_absolute_url(self):
         return reverse('post-detail', args=[self.pk])
->>>>>>> 121d478 (Initial Setup and Project Configuration for a Django Blog)
