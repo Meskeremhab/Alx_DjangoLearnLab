f3652474bf461618aabaff493f5e51ef92049f62
mhabtom <meski123habtom@gmail.com>
2025-10-11 16:38:41 +0200

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, FeedView
+
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -8,5 +9,5 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('feed/', include([])),  # keep if you already added a FeedView elsewhere
+    path('feed/', FeedView.as_view(), name='feed'), # keep if you already added a FeedView elsewhere
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,7 +3,7 @@ from rest_framework.exceptions import PermissionDenied
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
-
+from rest_framework import generics, permissions
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
 
@@ -27,3 +27,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # Optional: ensure you can only comment on visible posts; basic version:
         serializer.save(author=self.request.user)
+
+class FeedView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        # IDs of users I follow + myself
+        following_ids = list(self.request.user.following.values_list('id', flat=True))
+        following_ids.append(self.request.user.id)
+
+        return Post.objects.filter(author__in=following_ids).order_by('-created_at')
